895b1917d9c36952a6dab5968d97c4cc405fad4c
Laimis <laim0039@edu.easj.dk>
2023-02-05 19:57:38 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+
+                    // Translates CSS into CommonJS
+                    "css-loader",
+
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css',
+        })
+    ],
+    watch: true
+};
